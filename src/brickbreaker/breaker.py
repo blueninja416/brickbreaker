a4e65e81109bcfefc168a27e53d12f1362bfc3cd
Phoebe-Feathers <procyonparty@gmail.com>
2025-11-19 15:01:45 -0500

    Removed starting brick grid, added delay start

--- src/brickbreaker/breaker.py
+++ src/brickbreaker/breaker.py
@@ -69,6 +69,9 @@ class State:
     game_over: bool = False
     player_won: bool = False
 
+    breaker_delay_start_ms: int = 0
+    breaker_delay_active: bool = False
+
 
 S = State()
 
@@ -95,8 +98,8 @@ def make_demo_bricks():
     return bricks
 
 
-# Initial setup (no reset later)
-S.bricks = make_demo_bricks()
+# Initial Setup Removed, only needed for testing, game needs to start with no initial grid.
+# S.bricks = make_demo_bricks()
 
 
 def _serialize_bricks(bricks: list[Brick]) -> list[dict]:
@@ -108,6 +111,13 @@ def _serialize_bricks(bricks: list[Brick]) -> list[dict]:
 
 # Launch ball
 def launch_ball():
+    # Ball launch delay logic
+    if S.breaker_delay_active:
+        elapsed = (now_ms() - S.breaker_delay_start_ms) // 1000
+        if elapsed < 15:
+            return
+        else:
+            S.breaker_delay_active = False
     if S.launched or S.game_over or S.player_won:
         return
     S.launched = True
@@ -270,6 +280,10 @@ def _pump_incoming_host_for_sync(net):
                 continue  # ignore adds after round ends
             r = Brick(msg["x"], msg["y"], msg["w"], msg["h"], "red")
             S.bricks.append(r)
+            # 15 second delay until ball launch starts when placer places first brick
+            if not S.breaker_delay_active:
+                S.breaker_delay_active = True
+                S.breaker_delay_start_ms = now_ms()
             if not S.timer_running:
                 S.timer_running = True
                 S.time_start_ms = now_ms()
